960974e6f60999b1bc736fcf34d91c7a59e53f41
kkishore07 <kanwalkishore24@gmail.com>
2025-09-15 10:21:55 +0530

    main function completed

---
+++ restaurant_management_system.java
@@ -0,0 +1,35 @@
+public class restaurant_management_system {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        RestaurantManagementSystem rms = new RestaurantManagementSystem();
+        rms.loadMenu("menu.txt");
+
+        while (true) {
+            System.out.println("\nRestaurant Management System");
+            System.out.println("1. Display Menu");
+            System.out.println("2. Place Order");
+            System.out.println("3. View Orders");
+            System.out.println("4. Exit");
+            System.out.print("Choose an option: ");
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    rms.displayMenu();
+                    break;
+                case 2:
+                    rms.placeOrder(scanner);
+                    break;
+                case 3:
+                    rms.viewOrders();
+                    break;
+                case 4:
+                    System.out.println("Exiting...");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
+    }
+}
